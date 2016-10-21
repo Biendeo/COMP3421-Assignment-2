5be729033b559c84b62d5c4500b4e2be66485a29
Thomas Moffet <pantherman1996@gmail.com>
2016-10-22 10:11:39 +1100

    Shaders are all good. Specular mapping has been added to the cat. I can also confirm this runs on CSE computers.

--- res/shaders/cat_frag.glsl
+++ res/shaders/cat_frag.glsl
@@ -1,4 +1,7 @@
-#version 130
+#version 120
+
+// Based on the shader from here:
+// http://www.fabiensanglard.net/bumpMapping/index.php
 
 uniform sampler2D diffuseTexture;
 uniform sampler2D normalTexture;
@@ -12,37 +15,33 @@ varying vec3 eyeVec;
 
 void main() {
 
-    // lookup normal from normal map, move from [0,1] to  [-1, 1] range, normalize
-    vec3 normal = 2.0 * texture2D (normalTexture, gl_TexCoord[0].st).rgb - 1.0;
-    normal = normalize (normal);
+	// lookup normal from normal map, move from [0,1] to  [-1, 1] range, normalize
+	vec3 normal = 2.0 * texture2D(normalTexture, gl_TexCoord[0].st).rgb - 1.0;
+	normal = normalize (normal);
 
-    // compute diffuse lighting
-    float lamberFactor= max (dot (lightVec, normal), 0.0) ;
-    vec4 diffuseMaterial = vec4(0.0);
-    vec4 diffuseLight  = vec4(0.0);
+	vec4 specular = texture2D(specularTexture, gl_TexCoord[0].st).rgba;
 
-    // compute specular lighting
-    vec4 specularMaterial ;
-    vec4 specularLight ;
-    float shininess ;
+	// compute diffuse lighting
+	float lambertFactor = max (dot (lightVec, normal), 0.0);
+	vec4 diffuseMaterial = vec4(0.0);
+	vec4 diffuseLight = vec4(0.0);
 
-    // compute ambient
-    vec4 ambientLight = gl_LightSource[0].ambient;
+	// compute specular lighting
+	vec4 specularMaterial = vec4(1.0);
+	vec4 specularLight = gl_LightSource[0].specular;
+	float shininess = pow(max(dot(halfVec, normal), 0.0), 2.0);
 
-    if (lamberFactor > 0.0) {
-        diffuseMaterial = texture2D (diffuseTexture, gl_TexCoord[0].st);
-        diffuseLight  = gl_LightSource[0].diffuse;
+	// compute ambient
+	vec4 ambientLight = gl_LightSource[0].ambient;
 
-        // In doom3, specular value comes from a texture
-        specularMaterial =  vec4(1.0)  ;
-        specularLight = gl_LightSource[0].specular;
-        shininess = pow (max (dot (halfVec, normal), 0.0), 2.0)  ;
+	if (lambertFactor > 0.0) {
+		diffuseMaterial = texture2D (diffuseTexture, gl_TexCoord[0].st);
+		diffuseLight = gl_LightSource[0].diffuse;
 
-        gl_FragColor =	diffuseMaterial * diffuseLight * lamberFactor ;
-        gl_FragColor +=	specularMaterial * specularLight * shininess ;
+		gl_FragColor = diffuseMaterial * diffuseLight * lambertFactor;
+		gl_FragColor += specularMaterial * specularLight * shininess;
+	}
 
-    }
-
-    gl_FragColor +=	ambientLight;
+	gl_FragColor += ambientLight;
 
 }
